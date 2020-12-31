c78e7dd04607dd757896339c7f05c4efa459bee0
Ravi Kiran Moguluri <ravikiran.378@Gmail.Com>
2020-12-31 12:53:02 +0530

    Update Application.java
    Dummy Commit to fire jenkins build

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-//@SpringBootApplication
+//@SpringBootApplication 
 @EnableAutoConfiguration
 @Configuration
 //@EnableWebMvc
